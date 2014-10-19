f973c9e03aa76040ee0b90cac240c6181b7bbd69
jensenr30 <jensenr30@gmail.com>
2014-10-18 19:57:59 -0500

    % Alt+mouse no longer crashes the game. (panning works again)

--- main.c
+++ main.c
@@ -135,8 +135,7 @@ int main( int argc, char* args[] )
 					verify_camera();
 
 					//reset the user's curcor position to the original position the curcor was in when the user started panning the camera
-					//SDL_WarpMouse(mouse_x_when_pan, mouse_y_when_pan);
-					SDL_WarpMouseInWindow(screen, mouse_x_when_pan, mouse_y_when_pan);
+					SDL_WarpMouseInWindow(window, mouse_x_when_pan, mouse_y_when_pan);
 				}
             }
             else if(event.type == SDL_WINDOWEVENT_RESIZED ){							/// window resize
